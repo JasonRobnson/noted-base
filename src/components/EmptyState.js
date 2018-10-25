e1a2b851aa7a511659e7b084ae7ff8c6b590f3b3
unknown <bubbastinx@gmail.com>
2018-10-25 11:23:37 -0500

    changed css and emptystate

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -5,9 +5,7 @@ import Styles from '../styles.css';
 const EmptyState = () => (
   <article className={Styles.emptystate}>
     <h1>Welcome</h1>
-    <p>
-      Select a note from the sidebar to get started.
-    </p>
+    <p>Select a note from the sidebar to get started. I wonder what else this does.</p>
   </article>
 );
 
